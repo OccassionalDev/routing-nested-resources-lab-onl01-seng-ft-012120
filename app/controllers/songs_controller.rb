11c535e802185eab3c1705dcaf5f694fad7abb6c
Erica Schimmel <ericaschimmel01@gmail.com>
2020-04-10 04:53:13 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,9 +2,14 @@ class SongsController < ApplicationController
   def index
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
-    else 
+      if @artist.nil?
+        redirect_to artists_path, alert: "Artist not found"
+      else
+        @songs = @artist.songs
+      end
+    else
       @songs = Song.all
-    end 
+    end
   end
 
   def show
